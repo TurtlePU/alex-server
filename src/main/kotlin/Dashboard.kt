3cb8ae0a41d4beff7c3750329870382e83ed465a
TurtlePU <sokolov.p64@gmail.com>
2021-10-02 16:24:25 +0300

    better server termination, table layout

--- src/main/kotlin/Dashboard.kt
+++ src/main/kotlin/Dashboard.kt
@@ -1,5 +1,6 @@
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import javafx.collections.ObservableList
 import tornadofx.*
 import java.net.InetAddress
 
@@ -7,30 +8,62 @@ class Dashboard : View("My View") {
     private val controller: DashboardController by inject()
 
     override val root = borderpane {
-        top = menubar {
-            menu("File") {
-                item("Quit", "Shortcut+Shift+Q").action {
-                    replaceWith<Results>()
-                    close()
+        top = label(controller.serverAddress)
+        center = tableview(controller.participants) {
+            readonlyColumn("Name", Participant::name)
+            for (jury in controller.grading) {
+                column<Participant, Double?>(jury.key.name) {
+                    jury.value[it.value]!!
                 }
             }
-            menu(controller.serverAddress)
+            column("Total", controller::countTotal)
         }
-        bottom = button(messages["preview.results"]) {
-            action { openInternalWindow(controller.createPreview()) }
+        bottom = hbox {
+            button("+") {
+                action { openInternalWindow(controller.participantForm()) }
+            }
+            button(messages["preview.results"]) {
+                action { openInternalWindow(controller.previewResults()) }
+            }
         }
     }
 
-    init {
-        whenDocked { controller.startServer() }
-        whenUndocked { controller.stopServer() }
+    override fun onDock() {
+        controller.startServer()
+        currentWindow!!.setOnCloseRequest {
+            onUndock()
+        }
+    }
+
+    override fun onUndock() {
+        controller.stopServer()
     }
 }
 
-class DashboardController : Controller() {
+class DashboardController(val participants: ObservableList<Participant>, jury: List<Jury>) : Controller() {
     val serverAddress: String get() = "${InetAddress.getLocalHost().hostName}:$port"
+    val grading = jury.associateWith {
+        participants
+            .associateWith { objectProperty<Double?>() }
+            .toMutableMap()
+            .apply { participants.onChange { event ->
+                event.removed?.forEach { remove(it) }
+                event.addedSubList?.let { putAll(it.associateWith { objectProperty() }) }
+            } }
+    }
 
-    fun createPreview() = find<Results>()
+    fun countTotal(participant: Participant): Double? {
+        val grades = grading.values.mapNotNull { it[participant]!!.value }
+        return if (grades.isEmpty()) null else grades.sum() / grades.size
+    }
+
+    fun participantForm(): View {
+        val scope = Scope()
+        setInScope(ParticipantModel(participants::add), scope)
+        return find<ParticipantForm>(scope)
+    }
+
+    fun previewResults() = find<Results>()
 
     fun startServer() = runAsync { server.start() }
 
@@ -39,3 +72,5 @@ class DashboardController : Controller() {
     private val port = 8080
     private val server = embeddedServer(Netty, port = port) {}
 }
+
+data class Jury(val name: String)
